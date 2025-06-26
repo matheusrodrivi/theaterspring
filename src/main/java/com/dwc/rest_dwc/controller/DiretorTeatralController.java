99080695dccd4e0c4eb140f18313215562c515bb
matheusrodrivi <matheusrovicontato@gmail.com>
2025-06-26 10:06:19 -0300

    Updated: Spring documentation in DiretorTeatralController and PecaTeatralController

--- src/main/java/com/dwc/rest_dwc/controller/DiretorTeatralController.java
+++ src/main/java/com/dwc/rest_dwc/controller/DiretorTeatralController.java
@@ -5,6 +5,9 @@ import com.dwc.rest_dwc.service.DiretorTeatralService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+
 import java.util.List;
 
 @RestController
@@ -18,39 +21,37 @@ public class DiretorTeatralController {
     }
 
     // Leitura de um DiretorTeatral especifico
+    @Operation(summary = "Buscar DiretorTeatral por ID", description = "Retorna os dados de um Diretor Teatral específico com base no ID informado.")
     @GetMapping("{id}")
     public DiretorTeatral getDiretorTeatral(@PathVariable("id") int id) {
         return diretorTeatralService.getDiretorTeatral(id);
     }
 
     // Leitura de todos DiretorTeatral
+    @Operation(summary = "Buscar todos os dados de DiretorTeatral", description = "Retorna todos os dados de Diretor Teatral.")
     @GetMapping()
     public List<DiretorTeatral> getAllDiretorTeatral() {
         return diretorTeatralService.getAllDiretorTeatral();
     }
 
-
+    @Operation(summary = "Criar um DiretorTeatral", description = "Cria um DiretorTeatral com POST")
     @PostMapping
     public String createDiretorTeatral(@RequestBody DiretorTeatral diretorTeatral) {
         diretorTeatralService.createDiretorTeatral(diretorTeatral);
         return "Diretor teatral criado";
     }
 
+    @Operation(summary = "Atualiza um DiretorTeatral", description = "Atualiza um DiretorTeatral com PUT")
     @PutMapping
     public String updateDiretorTeatral(@RequestBody DiretorTeatral diretorTeatral) {
         diretorTeatralService.updateDiretorTeatral(diretorTeatral);
         return "Diretor teatral atualizado com sucesso";
     }
 
+    @Operation(summary = "Deleta um DiretorTeatral", description = "Deleta um DiretorTeatral com DELETE")
     @DeleteMapping("{id}")
     public String deleteDiretorTeatral(@PathVariable("id") int id) {
         diretorTeatralService.deleteDiretorTeatral(id);
         return "Diretor teatral excluido com sucesso";
     }
-
-
-
-
-
-
 }
--- src/main/java/com/dwc/rest_dwc/controller/PecaTeatralController.java
+++ src/main/java/com/dwc/rest_dwc/controller/PecaTeatralController.java
@@ -4,6 +4,9 @@ import com.dwc.rest_dwc.model.PecaTeatral;
 import com.dwc.rest_dwc.service.PecaTeatralService;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+
 import java.util.List;
 
 @RestController
@@ -16,28 +19,33 @@ public class PecaTeatralController {
         this.pecaTeatralService = pecaTeatralService;
     }
 
+    @Operation(summary = "Buscar PecaTeatral por ID", description = "Retorna os dados de uma PecaTeatral específico com base no ID informado.")
     @GetMapping("{id}")
     public PecaTeatral getPecaTeatral (@PathVariable("id") int id) {
         return pecaTeatralService.getPecaTeatral(id);
     }
 
+    @Operation(summary = "Buscar todos os dados PecaTeatral", description = "Retorna todos os dados de uma PecaTeatral.")
     @GetMapping
     public List<PecaTeatral> getAllPecaTeatral() {
         return pecaTeatralService.getAllPecaTeatral();
     }
 
+    @Operation(summary = "Criar PecaTeatral", description = "Cria PecaTeatral com POST.")
     @PostMapping
     public String createPecaTeatral(@RequestBody PecaTeatral pecaTeatral) {
         pecaTeatralService.createPecaTeatral(pecaTeatral);
         return "Peça teatral criada";
     }
 
+    @Operation(summary = "Atualizar PecaTeatral", description = "Atualizar os dados de uma PecaTeatral.")
     @PutMapping
     public String updatePecaTeatral(@RequestBody PecaTeatral pecaTeatral) {
         pecaTeatralService.updatePecaTeatral(pecaTeatral);
         return "Peça teatral atualizada com sucesso";
     }
 
+    @Operation(summary = "Deletar PecaTeatral", description = "Deletar os dados de uma PecaTeatral.")
     @DeleteMapping("{id}")
     public String deletePecaTeatral(@PathVariable("id") int id) {
         pecaTeatralService.deletePecaTeatral(id);
